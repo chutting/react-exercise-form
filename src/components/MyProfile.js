464da8147f309e19a9ca2ac7caaa193672d55f2d
chutting <tingting.chu@thoughtworks.com>
2020-09-23 12:51:49 +0800

    [Chu] feat: complete the submit feature

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,18 +2,45 @@ import React, {Component} from 'react';
 import './myProfile.less';
 
 class MyProfile extends Component {
+
+
+  state = {
+    name: "",
+    gender: "male",
+    description: "",
+    haveReadConduct: false
+  }
+
+  handleChange = (field, event) => {
+    this.setState({
+      [field]: event.target.value
+    })
+  }
+
+  handleChecked = (event) => {
+    this.setState({
+      haveReadConduct: event.target.checked
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state)
+  }
   render() {
     return <div className = "profile">
       <h1>My Profile</h1>
       <form className="profile-form">
         <section className="profile-item">
           <label htmlFor = "name">Name</label>
-          <input className="name" type="text" placeholder="Your name"/>
+          <input className="name" type="text" placeholder="Your name"
+                 value={this.state.name}
+                 onChange = {(event) => this.handleChange("name", event)}/>
         </section>
 
         <section className="profile-item" id="gender-select">
           <label htmlFor = "gender">Gender</label>
-          <select className="gender">
+          <select className="gender" onChange={(event) => this.handleChange("gender", event)}>
             <option value="male">Male</option>
             <option value="female">Female</option>
           </select>
@@ -21,15 +48,23 @@ class MyProfile extends Component {
 
         <section className="profile-item">
           <label htmlFor = "description">Description</label>
-          <textarea name="description" cols="40" rows="5" placeholder="Description about yourself"></textarea>
+          <textarea name="description" cols="40" rows="5"
+                    placeholder="Description about yourself"
+                    onChange={ event => this.handleChange("description", event)}></textarea>
         </section>
 
         <section className="profile-item" id="conduct">
-          <input className="conduct-checkbox" type="checkbox" value="conduct-check"/>
+          <input className="conduct-checkbox"
+                 type="checkbox"
+                 value="conduct-check"
+                 onChange={event => this.handleChecked(event)}/>
           <label htmlFor = "conduct-checkbox" id="conduct-label">I have read the terms of conduct</label>
         </section>
 
-        <button type="submit" className="submit">Submit</button>
+        <input type="submit" className="submit"
+               onClick={(event) => this.handleSubmit(event)}
+               disabled={!this.state.name || !this.state.description || !this.state.haveReadConduct}
+               value="Submit"/>
       </form>
     </div>
   }
